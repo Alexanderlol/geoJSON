d1d9bfb3ded4caa8601cb7aafcb2f6d05f877967
Alex Afeltra <afeltra.alex@gmail.com>
2018-02-25 18:10:09 -0500

    create map

--- app.js
+++ app.js
@@ -2,8 +2,28 @@
 var chart_width     =   800;
 var chart_height    =   600;
 
+// Projection
+var projection = d3.geoAlbersUsa()
+		.scale([chart_width])
+		.translate([ chart_width / 2, chart_height / 2 ]);
+
+var path = d3.geoPath(projection);
+		//.projection(projection);
+
 // Create SVG
 var svg             =   d3.select("#chart")
     .append("svg")
     .attr("width", chart_width)
     .attr("height", chart_height);
+
+// Data
+d3.json('us.json', function( data ) {
+	svg.selectAll('path')
+			.data(data.features)
+			.enter()
+			.append('path')
+			.attr('d', path)
+			.attr('fill', '#58CCE1')
+			.attr('stroke', '#fff')
+			.attr('stroke-width', 1);
+});
