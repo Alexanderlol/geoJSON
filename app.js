ac090ca5c24543091d46ca45b81d54f790570871
Alex Afeltra <afeltra.alex@gmail.com>
2018-02-25 19:00:01 -0500

    chloropleth map

--- app.js
+++ app.js
@@ -10,20 +10,57 @@ var projection = d3.geoAlbersUsa()
 var path = d3.geoPath(projection);
 		//.projection(projection);
 
+var color = d3.scaleQuantize().range([
+'rgb(255,245,240)',
+'rgb(254,224,210)',
+'rgb(252,187,161)',
+'rgb(252,146,114)',
+'rgb(251,106,74)',
+'rgb(239,59,44)',
+'rgb(203,24,29)',
+'rgb(165,15,21)',
+'rgb(103,0,13)'
+]);
+
 // Create SVG
 var svg             =   d3.select("#chart")
     .append("svg")
     .attr("width", chart_width)
     .attr("height", chart_height);
 
-// Data
-d3.json('us.json', function( data ) {
-	svg.selectAll('path')
-			.data(data.features)
-			.enter()
-			.append('path')
-			.attr('d', path)
-			.attr('fill', '#58CCE1')
-			.attr('stroke', '#fff')
-			.attr('stroke-width', 1);
+d3.json('zombie-attacks.json', function(zombie_data){
+	color.domain([
+		d3.min(zombie_data, function(d){
+			return d.num;
+		}),
+		d3.max(zombie_data, function(d){
+			return d.num;
+		})
+	]);
+	// Map Data
+	d3.json('us.json', function( us_data ) {
+		us_data.features.forEach(function(us_e, us_i){
+			zombie_data.forEach(function(z_e, z_i){
+				if(us_e.properties.name !== z_e.state){
+					return null;
+				}
+
+				us_data.features[us_i].properties.num = parseFloat(z_e.num);
+			});
+		});
+
+		//console.log(us_data);
+		
+		svg.selectAll('path')
+				.data(us_data.features)
+				.enter()
+				.append('path')
+				.attr('d', path)
+				.attr('fill', function(d){
+					var num = d.properties.num;
+					return num ? color( num ) : '#ddd';
+				})
+				.attr('stroke', '#fff')
+				.attr('stroke-width', 1);
+	});
 });
